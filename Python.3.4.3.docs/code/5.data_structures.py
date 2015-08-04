f27cae066693b500aa310739d2db26230c0292fe
guochangjiang <xuezhe@outlook.com>
2015-08-04 15:03:12 +0800

    update

---
+++ Python.3.4.3.docs/code/5.data_structures.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+## list methond examples
+
+a = [66.25, 333, 333, 1, 1234.5]
+print(a.count(333), a.count(66.25))
+a.insert(2, -1)
+a.append(333)
+print(a)
+print(a.index(333))
+a.remove(333)
+print(a)
+a.reverse()
+print(a)
+a.sort()
+print(a)
+print(a.pop())
+print(a)
